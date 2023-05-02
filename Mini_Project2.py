5087b1b3a528c4e5e97cc4f08a23a6e20a1cdac9
ElDon2308 <101525418+ElDon2308@users.noreply.github.com>
2023-05-02 15:48:11 -0400

    added comments

--- Mini_Project2.py
+++ Mini_Project2.py
@@ -15,78 +15,101 @@ compare the prices(def create) of each property and tell the user whats the best
 '''
 #Project part 2
 
-#Importing needed libraries
+# Importing needed libraries
 import pandas as pd
 
-#Final Var for tax
-FINAL_TAX= .72
+# Final Var for tax
+FINAL_TAX = 0.72
+
 
 def properties():
     # This will read from the properties file
     try:
         propDF = pd.read_csv('properties.csv')
-        
+
         # Convert Price column to string
         propDF['Price'] = propDF['Price'].astype(str)
-        
+
         # Remove special chars
         propDF['Price'] = propDF['Price'].str.replace('[$|,]', '', regex=True)
-        
+
         # Converts the price column to all numeric values
         propDF['Price'] = pd.to_numeric(propDF['Price'])
-        
+
         return propDF
-    
+
     except FileNotFoundError as err:
         print('404: ', err)
-    
+
     except EOFError:
         print("End of file reached!")
 
 
 def calculateAssesVal(propDF):
-    
+
     assesVal = propDF['Price'] * 0.6
-    
+
     return assesVal
 
 
 def propTax(assesVal):
-    
+
     calc = assesVal / 100
-    
+
     prop_tax = FINAL_TAX * calc
-    
+
     return prop_tax
-    
-  
+
+
 def updateFile():
     propDF = properties()
-    
+
     propDF['Assessment Value'] = calculateAssesVal(propDF)
-    
+
     propDF['Property Tax'] = propTax(propDF['Assessment Value'])
 
     propDF.to_csv('properties.csv', index=False)
-    
 
 
 def main():
+    # Update properties file
     updateFile()
-    df= pd.read_csv('properties.csv')
-    num_rows= df.shape[0]
+
+    # Read properties file
+    df = pd.read_csv('properties.csv')
+
+    # Get the number of rows in the dataframe
+    num_rows = df.shape[0]
+
+    # Loop through each row in the dataframe
     for i in range(num_rows):
+        # Get the row
         row = df.loc[i]
+        
+        # Get the property name, assessment value, and property tax
         prop = row['Municipality']
-        aVal= row['Assessment Value']
-        tx= row['Property Tax']   
-        message= f'{prop} has an assessment value of ${aVal:,.2f} and a property tax of ${tx:.2f}'
-        print(message)
-  
-main() 
+        aVal = row['Assessment Value']
+        tx = row['Property Tax']
+
+        # Create message
+        message = f'{prop} has an assessment value of ${aVal:,.2f} and a property tax of ${tx:.2f}'
+        
+        # Create list to hold messages
+        prop_List = []
+        
+        # Append message to list
+        prop_List.append(message)
+        
+        # Print the list of messages
+        print(prop_List)
+
+
+# Call the main function
+main()
     
 
 
+#code that I didn't use 
 '''
 def prop_assessment():
     pass
